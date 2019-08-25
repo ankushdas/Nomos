1a0dc34c46dc1851dbc0347b6acee102fe761684
ankushdas <das.ankush.8@gmail.com>
2019-08-25 13:07:39 -0700

    tok stream interface fixed

--- TokStream.mli
+++ TokStream.mli
@@ -1,5 +1,3 @@
 type 'a front = Cons of 'a * (unit -> 'a front) | Nil
 and 'a stream = unit -> 'a front
 val force : (unit -> 'a) -> 'a
-val fromList : 'a list -> unit -> 'a front
-val isNil : 'a front -> bool
