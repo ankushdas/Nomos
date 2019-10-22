901ec76044ee076f3767b4cdcff2ca897dba7e0b
Ishani Santurkar <ishani.santurkar@gmail.com>
2019-10-22 00:19:32 -0400

    Committing previously written unification code

--- OcamlParGen/test.ml
+++ OcamlParGen/test.ml
@@ -5,4 +5,4 @@ let x : int = 5 in x : int;;
 let x : int list = [5,6,7] in match x : int list with [] -> [5] | y::ys -> ys : int list;;
 app(app((fun (m : int) (n : int) -> m + n) : int -> int -> int, 5 : int) : int -> int, 4 : int) : int;;
 app(if true then (fun (m : int) -> m) else (fun (x : int) -> x + 1) : int -> int, 7 : int) : int;;
-if ((1 + 2 = 3) && true) then 5 else 6 : int
+if ((1 + 2 = 5) || (5*7 <> 35)) then 7*8 else 6/3 : int
