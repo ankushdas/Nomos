977ef231ed4237a08647b006b86ebfea5bfc26aa
Ankush Das <ankushdas@fb.com>
2020-01-17 13:24:10 -0500

    forgot to add files

---
+++ nomos-src/NomosFlags.mli
@@ -0,0 +1,14 @@
+(* Interface for Flags *)
+
+type cost = None | Free | Recv | RecvSend | Send
+val parseCost : string -> cost option
+val pp_cost : cost -> string
+type syntax = Implicit | Explicit
+val parseSyntax : string -> syntax option
+val pp_syntax : syntax -> string
+val time : cost ref
+val work : cost ref
+val syntax : syntax ref
+val verbosity : int ref
+val help : bool ref
+val reset : unit -> unit
