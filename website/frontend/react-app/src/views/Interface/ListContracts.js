9ce12d4608e39ba9c5c73347ccc2ff97256e282a
Jan Hoffmann <jhoffmann@cmu.edu>
2020-08-14 23:23:29 -0400

    list contracts

---
+++ website/frontend/react-app/src/views/Interface/ListContracts.js
@@ -0,0 +1,65 @@
+
+import React from "react";
+
+import {
+   Card,
+   CardHeader,
+   CardTitle,  
+   CardBody,
+   Table
+} from "reactstrap";
+
+import ModalButton from "./ModalButton.js"
+
+const printRow = arg =>
+   (
+      <tr>
+	 <td>{arg.channel}</td>
+	 <td className="text-center">
+	    <ModalButton
+		buttonLabel = "Show"
+                modalTitle = {"Type of Channel " + arg.channel}
+		modalBody = <pre>{arg.type}</pre>
+	       />
+	 </td>
+	 <td className="text-center">
+	    <ModalButton
+		buttonLabel = "Show"
+                modalTitle = {"Process Code @" + arg.channel}
+		modalBody = <pre>{arg.code}</pre>
+	       />
+	 </td>
+	 <td className="text-center">
+	    {String(arg.gas)}
+	 </td>
+      </tr>
+   )
+
+const ListContracts = props =>
+   (
+      <Card className="card-plain">
+	 <CardHeader>
+	    <CardTitle tag="h4">Blockchain State</CardTitle>
+	    <p className="category">The Current Blockchain State</p>
+	 </CardHeader>
+	 <CardBody>
+	    <div  style={{height:"35rem",overflowY:"auto"}}>
+	       <Table className="tablesorter" responsive>
+		  <thead className="text-primary">
+		     <tr>
+			<th>Channel Name</th>
+			<th className="text-center">Session Type</th>
+			<th className="text-center">Process Code</th>
+			<th className="text-center">Gas</th>		
+		     </tr>
+		  </thead>
+		  <tbody>
+		     {props.contList.map(printRow)}
+		  </tbody>
+	       </Table>
+	    </div>		  
+	 </CardBody>
+      </Card>
+   )
+
+export default ListContracts
