ea5c341e16ddee615ed748187e2368c35e82e368
ankushdas <das.ankush.8@gmail.com>
2020-08-24 17:29:45 -0400

    created Json functionality

---
+++ nomos-lib/JsonConfig.ml
@@ -0,0 +1,68 @@
+module TL = TopLevel
+module E = Exec
+module EM = ErrorMsg
+module F = NomosFlags
+
+(******************)
+(* JSON Functions *)
+(******************)
+
+let create_account initial_state account_name balance =
+  let state = initial_state in
+  let state = TL.create_account account_name state in
+  let state = TL.deposit_gas account_name balance state in
+  state;;
+
+let account_list state =
+  let (_tx, _ch, gas_accs, _types, _config) = state in
+  gas_accs;;
+
+let contract_list state =
+  let (_tx, _ch, _gas_accs, _types, config) = state in
+  let chantps = config.E.types in
+  chantps;;
+
+type elaboratedTxn = TSuccess of TL.environment | TFailure of string;;
+
+let type_check txn =
+  try
+    match TL.read_txn txn with
+        TL.RawTransaction env -> TSuccess(env)
+  with
+    EM.LexError m | EM.ParseError m | EM.TypeError m | EM.PragmaError m | EM.RuntimeError m ->
+      TFailure(m);;
+
+type output_state = BSuccess of E.blockchain_state | BFailure of string;;
+
+let submit state txn account_name =
+  try
+    let () = F.verbosity := -1 in
+    let () = TL.set_sender account_name in
+    let raw_env = TL.read_txn txn in
+    let env = TL.infer raw_env in
+    BSuccess(TL.run env state)
+  with
+    EM.LexError m | EM.ParseError m | EM.TypeError m | EM.PragmaError m | EM.RuntimeError m ->
+      BFailure(m);;
+(* need 
+
+  - main function
+
+    * parse json string input to json
+    * either error or one of the three valid cases
+    * parse state from sexp string
+    * call create_account, submit, or type_check
+    * create response json
+    * convert response json to string
+
+  - create_account (state:(ocaml type), account_name, balance) -> new_sate:(ocaml type)
+
+  - type_check (transaction:string) -> (elaborated_transaction:string)
+
+  - submit (state, elaborated_transaction, account) -> new_state
+
+  - account_list (state) -> json_acouunt_list
+
+  - contract_list (state) -> json_contract_list
+
+ *)
---
+++ nomos-lib/JsonConfig.mli
@@ -0,0 +1,19 @@
+module TL = TopLevel
+module E = Exec
+module EM = ErrorMsg
+module F = NomosFlags
+
+val create_account :
+  TL.E.blockchain_state -> string -> int -> TL.E.blockchain_state
+
+val account_list : 'a * 'b * 'c * 'd * 'e -> 'c
+
+val contract_list : 'a * 'b * 'c * 'd * E.configuration -> E.map_chan_tp
+
+type elaboratedTxn = TSuccess of TL.environment | TFailure of string
+
+val type_check : string -> elaboratedTxn
+
+type output_state = BSuccess of E.blockchain_state | BFailure of string
+
+val submit : TL.E.blockchain_state -> string -> string -> output_state
