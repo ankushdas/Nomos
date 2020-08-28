b013f5f6e30857e2626360af9883405568aa1be0
Jan Hoffmann <jhoffmann@cmu.edu>
2020-08-28 14:10:41 -0400

    read_txn now uses a in_channel instead of a string

--- nomos-lib/JsonConfig.mli
+++ nomos-lib/JsonConfig.mli
@@ -12,10 +12,10 @@ val contract_list : 'a * 'b * 'c * 'd * E.configuration -> E.map_chan_tp
 
 type elaboratedTxn = TSuccess of TL.environment | TFailure of string
 
-val type_check : string -> elaboratedTxn
+val type_check :  Stdlib.in_channel -> elaboratedTxn
 
 type output_state = BSuccess of E.blockchain_state | BFailure of string
 
-val submit : TL.E.blockchain_state -> string -> string -> output_state
+val submit : TL.E.blockchain_state -> Stdlib.in_channel -> string -> output_state
 
 val main : unit
