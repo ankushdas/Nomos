3f3b04729a28c8c0e94a73646a2bc4174214ebe8
Jan Hoffmann <jhoffmann@cmu.edu>
2020-08-30 14:38:48 -0400

    progress with json interface

--- nomos-lib/JsonConfig.ml
+++ nomos-lib/JsonConfig.ml
@@ -16,8 +16,7 @@ let blockchain_state_of_string str =
 let blockchain_state_to_string conf =
   C.Sexp.to_string (E.sexp_of_blockchain_state conf)
 
-type output_state = BSuccess of E.blockchain_state | BFailure of string;;
-  
+
 let account_list state =
   let (_tx, _ch, gas_accs, _types, _config) = state in
   let f (acc,balance) =
@@ -26,33 +25,63 @@ let account_list state =
   in
   `List (List.map f (C.Map.to_alist gas_accs))
 
+
+(* fixme: we need to get the code and the gas of a contract*)
+let contract_list state =
+  let (_tx, _ch, _gas_accs, _types, config) = state in
+  let chantps = config.E.types in
+  let f (channel,typ) =
+    `Assoc [ ("channel", `String (Pprint.pp_chan channel))
+           ; ("type", `String (Pprint.pp_tp_simple typ))
+           ; ("code", `String "")
+           ; ("gas", `String "") ]
+  in
+  `List (List.map f (C.Map.to_alist chantps))
+
+  
+let report_error response msg details =
+  let error =
+    `Assoc [ ("msg",`String msg)
+           ; ("details", `String details)]
+  in
+  `Assoc [("response",`String response)
+         ;("status", `String "error")
+         ;("error", error)]
+    
+
+  
 let create_account initial_state account_name balance =
   let state = TL.create_account account_name initial_state in
   let state = TL.deposit_gas account_name balance state in
   let str_state = blockchain_state_to_string state in
   let body =
   `Assoc [("state",`String str_state)
-         ;("accList", account_list state)]
+         ;("acclist", account_list state)]
   in
   `Assoc [("response",`String "create")
+         ;("status", `String "success")
          ;("body", body)]
 
 
-let contract_list state =
-  let (_tx, _ch, _gas_accs, _types, config) = state in
-  let chantps = config.E.types in
-  chantps;;
+(*Jan: I need to print a transaction at XX below but don't know how.*)
 
-type elaboratedTxn = TSuccess of TL.environment | TFailure of string;;
-
-let type_check txn =
+let type_check _state txn =
   try
-    match TL.read_txn txn with
-        TL.RawTransaction env -> TSuccess(env)
+    let TL.RawTransaction _txn' = TL.read_txn txn in
+    let body =
+      `Assoc [("transaction",`String "XX: I need to pring txn' here")
+             ;("gasbound", `Int (-99))]
+    in
+    `Assoc [("response",`String "typecheck")
+           ;("status", `String "success")
+           ;("body", body)]
   with
-    EM.LexError m | EM.ParseError m | EM.TypeError m | EM.PragmaError m | EM.RuntimeError m ->
-      TFailure(m);;
-
+    | EM.LexError m
+    | EM.ParseError m
+    | EM.TypeError m
+    | EM.PragmaError m
+    | EM.RuntimeError m ->
+       report_error "typecheck" m ""
 
 
 let submit state txn account_name =
@@ -61,12 +90,21 @@ let submit state txn account_name =
     let () = TL.set_sender account_name in
     let raw_env = TL.read_txn txn in
     let env = TL.infer raw_env in
-    BSuccess(TL.run env state)
+    let state = TL.run env state in
+    let str_state = blockchain_state_to_string state in
+    let body =
+      `Assoc [("state",`String str_state)
+             ;("contlist", contract_list state)
+             ;("acclist", account_list state)]
+    in
+    `Assoc [("response",`String "submit")
+           ;("status", `String "success")
+           ;("body", body)]
   with
       EM.LexError m | EM.ParseError m
     | EM.TypeError m | EM.PragmaError m | EM.RuntimeError m
     | EM.GasAcctError m | EM.FileError m ->
-                       BFailure(m);;
+                       report_error "submit" m ""
 
 exception Json_error of string
                       
@@ -87,34 +125,14 @@ let main =
        let balance = json_body |> J.Util.member "balance" |> J.Util.to_int in
        create_account initial_state account balance
     | "typecheck" ->
-       raise (EM.LexError "")
+       let txn = json_body |> J.Util.member "transaction" |> J.Util.to_string in
+       type_check initial_state txn
     | "submit" ->
-       raise (EM.LexError "")
+       let txn = json_body |> J.Util.member "transaction" |> J.Util.to_string in
+       let account = json_body |> J.Util.member "account" |> J.Util.to_string in
+       submit initial_state txn account
     | _ ->
        raise (Json_error ("Unknown request: " ^ request))
   in
   J.to_channel ?std:(Some true) stdout json_response
   
-
-  (* need 
-
-  - main function
-
-    * parse json string input to json
-    * either error or one of the three valid cases
-    * parse state from sexp string
-    * call create_account, submit, or type_check
-    * create response json
-    * convert response json to string
-
-  - create_account (state:(ocaml type), account_name, balance) -> new_sate:(ocaml type)
-
-  - type_check (transaction:string) -> (elaborated_transaction:string)
-
-  - submit (state, elaborated_transaction, account) -> new_state
-
-  - account_list (state) -> json_acouunt_list
-
-  - contract_list (state) -> json_contract_list
-
- *)
--- nomos-lib/JsonConfig.mli
+++ nomos-lib/JsonConfig.mli
@@ -3,14 +3,4 @@ module E = Exec
 module EM = ErrorMsg
 module F = NomosFlags
 
-type output_state = BSuccess of E.blockchain_state | BFailure of string
-
-val contract_list : 'a * 'b * 'c * 'd * E.configuration -> E.map_chan_tp
-
-type elaboratedTxn = TSuccess of TL.environment | TFailure of string
-
-val type_check : string -> elaboratedTxn
-
-val submit : TL.E.blockchain_state -> string -> string -> output_state
-
 val main : unit
