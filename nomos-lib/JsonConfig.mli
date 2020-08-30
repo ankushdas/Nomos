873d767fe76d172b1f98ef4045cf785b5305e40f
Jan Hoffmann <jhoffmann@cmu.edu>
2020-08-30 13:43:16 -0400

    merge

--- nomos-lib/JsonConfig.mli
+++ nomos-lib/JsonConfig.mli
@@ -9,8 +9,8 @@ val contract_list : 'a * 'b * 'c * 'd * E.configuration -> E.map_chan_tp
 
 type elaboratedTxn = TSuccess of TL.environment | TFailure of string
 
-val type_check :  Stdlib.in_channel -> elaboratedTxn
+val type_check : string -> elaboratedTxn
 
-val submit : TL.E.blockchain_state -> Stdlib.in_channel -> string -> output_state
+val submit : TL.E.blockchain_state -> string -> string -> output_state
 
 val main : unit
