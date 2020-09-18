e73d2a9dfd901bf2197411c8f98a8adc0bf700cd
Ankush Das <das.ankush.8@gmail.com>
2020-09-17 23:17:30 -0400

    updated the wallet

--- nomos-tests/test-wallet/run.sh
+++ nomos-tests/test-wallet/run.sh
@@ -5,8 +5,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 NOMOS="$DIR/../../"
 
-$NOMOS/_build/default/nomos-bin/nomos.exe -create -ts ankush -o s.conf
-$NOMOS/_build/default/nomos-bin/nomos.exe -create -ts ankush -i s.conf -o sa.conf
+$NOMOS/_build/default/nomos-bin/nomos.exe -create -ts ankush -o sa.conf
 $NOMOS/_build/default/nomos-bin/nomos.exe -deposit 1000 -ts ankush -i sa.conf -o s0.conf
 $NOMOS/_build/default/nomos-bin/nomos.exe -w send -ts ankush -i s0.conf -o s1.conf "$DIR/t1.nom"
 $NOMOS/_build/default/nomos-bin/nomos.exe -w send -ts ankush -i s1.conf -o s2.conf "$DIR/t2.nom"
