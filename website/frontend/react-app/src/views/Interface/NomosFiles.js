181caca7fd468213d9b6c2129dc92ab0366aa464
ankushdas <das.ankush.8@gmail.com>
2020-09-21 15:09:30 -0400

    bug fix in type name

--- website/frontend/react-app/src/views/Interface/NomosFiles.js
+++ website/frontend/react-app/src/views/Interface/NomosFiles.js
@@ -12,13 +12,13 @@ import auctionT1 from "assets/nomos/auction-contract-t1.nom"
 import auctionT2 from "assets/nomos/auction-bid-t2.nom"
 import auctionT3 from "assets/nomos/auction-bid-t3.nom"
 import auctionT4 from "assets/nomos/auction-bid-t4.nom"
-import auctionT5 from "assets/nomos/auction-bid-t5.nom"
+import auctionT5 from "assets/nomos/auction-collect-t5.nom"
 
 const nomosFiles = [
   { name: "Storage: Txn 1", file: storageT1},
   { name: "Storage: Txn 2", file: storageT2},
   { name: "Storage: Txn 3", file: storageT3},
-  { name: "Storage: Txn 3", file: storageT4},
+  { name: "Storage: Txn 4", file: storageT4},
   { name: "Wallet: Txn 1", file: walletT1},
   { name: "Wallet: Txn 2", file: walletT2},
   { name: "Wallet: Txn 3", file: walletT3},
