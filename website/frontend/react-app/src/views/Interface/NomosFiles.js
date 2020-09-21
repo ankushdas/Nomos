b5616a3bb5f33480fab06f72e30f169c35c9825d
ankushdas <das.ankush.8@gmail.com>
2020-09-21 14:47:46 -0400

    updated Nomos files

--- website/frontend/react-app/src/views/Interface/NomosFiles.js
+++ website/frontend/react-app/src/views/Interface/NomosFiles.js
@@ -1,45 +1,33 @@
+import storageT1 from "assets/nomos/storage-contract-t1.nom"
+import storageT2 from "assets/nomos/get-storage-t2.nom"
+import storageT3 from "assets/nomos/set-storage-t3.nom"
+import storageT4 from "assets/nomos/get-storage-t4.nom"
 
-import append from "assets/nomos/append.nom"
-import auctionDemo from "assets/nomos/auction-demo.nom"
-import auctionDict from "assets/nomos/auction-dict.nom"
-import auctionPhases from "assets/nomos/auction-phases.nom"
-import auction from "assets/nomos/auction.nom"
-import bankAccount from "assets/nomos/bank-account.nom"
-import bracha from "assets/nomos/bracha.nom"
-import dictMoney from "assets/nomos/dict-money.nom"
-import dictionary from "assets/nomos/dictionary.nom"
-import erc20 from "assets/nomos/erc20.nom"
-import escrow from "assets/nomos/escrow.nom"
-import insurance from "assets/nomos/insurance.nom"
-import puzzle from "assets/nomos/puzzle.nom"
-import simpleWait from "assets/nomos/simple-wait.nom"
-import simple from "assets/nomos/simple.nom"
-import votingPot from "assets/nomos/voting-pot.nom"
-import voting from "assets/nomos/voting.nom"
-import walletDemo from "assets/nomos/wallet-demo.nom"
-import wallet from "assets/nomos/wallet.nom"
+import walletT1 from "assets/nomos/create-wallet-t1.nom"
+import walletT2 from "assets/nomos/create-wallet-t2.nom"
+import walletT3 from "assets/nomos/transfer-t3.nom"
+import walletT4 from "assets/nomos/print-balance-t4.nom"
 
+import auctionT1 from "assets/nomos/auction-contract-t1.nom"
+import auctionT2 from "assets/nomos/auction-bid-t2.nom"
+import auctionT3 from "assets/nomos/auction-bid-t3.nom"
+import auctionT4 from "assets/nomos/auction-bid-t4.nom"
+import auctionT5 from "assets/nomos/auction-bid-t5.nom"
 
 const nomosFiles = [
-  { name: "append", file: append},
-  { name: "auction-demo", file: auctionDemo},
-  { name: "auction-dict", file: auctionDict},
-  { name: "auction-phases", file: auctionPhases},
-  { name: "auction", file: auction},
-  { name: "bank-account", file: bankAccount},
-  { name: "bracha", file: bracha},
-  { name: "dict-money", file: dictMoney},
-  { name: "dictionary", file: dictionary},
-  { name: "erc20", file: erc20},
-  { name: "escrow", file: escrow},
-  { name: "insurance", file: insurance},
-  { name: "puzzle", file: puzzle},
-  { name: "simple-wait", file: simpleWait},
-  { name: "simple", file: simple},
-  { name: "voting-pot", file: votingPot},
-  { name: "voting", file: voting},
-  { name: "wallet-demo", file: walletDemo},
-  { name: "wallet", file: wallet}
+  { name: "Storage: Txn 1", file: storageT1},
+  { name: "Storage: Txn 2", file: storageT2},
+  { name: "Storage: Txn 3", file: storageT3},
+  { name: "Storage: Txn 3", file: storageT4},
+  { name: "Wallet: Txn 1", file: walletT1},
+  { name: "Wallet: Txn 2", file: walletT2},
+  { name: "Wallet: Txn 3", file: walletT3},
+  { name: "Wallet: Txn 4", file: walletT4},
+  { name: "Auction: Txn 1", file: auctionT1},
+  { name: "Auction: Txn 2", file: auctionT2},
+  { name: "Auction: Txn 3", file: auctionT3},
+  { name: "Auction: Txn 4", file: auctionT4},
+  { name: "Auction: Txn 5", file: auctionT5}
 ]
 
 
